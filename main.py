201c2656f9cdbb82bd392b39615cf396f0c74bc0
VenkateshL1921 <venkyslotlikar19@gmail.com>
2023-11-06 18:06:17 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.classifier import logger
+from classifier import logger
+from classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logging setup")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
