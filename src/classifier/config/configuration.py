21746e69bcaaa22d3f008e39c400a12f414910b3
VenkateshL1921 <venkyslotlikar19@gmail.com>
2023-11-13 20:27:32 +0530

    Improved model performance

--- src/classifier/config/configuration.py
+++ src/classifier/config/configuration.py
@@ -59,7 +59,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney scans")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney scans full")
         create_directories([
             Path(training.root_dir)
         ])
@@ -81,7 +81,7 @@ class ConfigurationManager:
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
-            training_data="artifacts/data_ingestion/kidney scans",
+            training_data="artifacts/data_ingestion/kidney scans full",
             mlflow_uri="https://dagshub.com/venkyslotlikar19/Disease-classification-Using-Deep-Learning-MLOps.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
